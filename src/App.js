8872c4239b43a9728a48746889d18da80e255b38
Jeanramos500 <Jean_2009.ramos@hotmail.com>
2020-06-08 20:24:49 +0000

    Front-end

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import api from './api';
 
-const lista=[
+/*const lista=[
     {id: 1,name:'Estudar', done: false},
     {id: 2,name:'Comer', done: false},
     {id: 3,name:'Descansar', done: true},
-]
+]*/
 
 
 function App() {
 
+    const [lista, setLista] = useState([]); //Imutabilidade
+
+    useEffect(()=>{
+        api.get('/tarefas').then((response) =>{
+            const itens = response.data;
+            setLista(itens);
+        })
+    },[])
+    
     return (
     <table>
         {lista.map(item => (
-        <tr>
-            <td>id</td>
-            <td>name</td>
-            <td>done</td>
+        <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>
+                <input type="checkbox" checked={item.done}/>
+            </td>
         </tr>
     ))}
     </table>
