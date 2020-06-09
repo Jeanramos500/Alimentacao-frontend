1634213ffbf0a0a656c526a18493d2c1186baf44
Jeanramos500 <Jean_2009.ramos@hotmail.com>
2020-06-09 18:33:13 +0000

    Front-end

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, {useState,useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import api from './api';
+import Button from '@material-ui/core/Button';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function App() {
 
     const [lista, setLista] = useState([]); //Imutabilidade
+    const [loading, setLoading] = useState(true);
 
+ 
+    //executa para obeter informações externas.
     useEffect(()=>{
         api.get('/alimentacao').then((response) =>{
             const itens = response.data;
             setLista(itens);
+            setLoading(false);
         })
     },[])
     
     return (
-    <table>
-        {lista.map(item => (
-        <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>{item.nome}</td>
-            <td>{item.quantidade}</td>
-            <td>{item.gramas}</td>
-        </tr>
-    ))}
-    </table>
+    <div style={{marginTop: '80px'}}>
+{ loading ? <CircularProgress/> : <div/> }
+        <Table>
+            <TableBody>
+                {lista.map(item => (
+                <TableRow key={item.id}>
+                    <TableCell>{item.id}</TableCell>
+                    <TableCell>{item.nome}</TableCell>
+                    <TableCell>{item.quantidade}</TableCell>
+                    <TableCell>{item.gramas}</TableCell>
+                </TableRow>
+                 ))}
+             </TableBody>
+        </Table>
+        <Link to="/create">Adicionar</Link><Button variant="contained" color="primary">
+            Primary
+        </Button>
+    </div>
     );
         }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import {BrowserRouter, Switch, Route}from 'react-router-dom';
+import App from './App';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route path="/">
-                <App />
-            </Route>
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Typography variant="h6">
+                        Lista alimentacao
+                    </Typography>
+                </Toolbar>
+        </AppBar>
         </Switch>
+            <Route path="/" exact>
+                <App/>
+            </Route>
     </BrowserRouter>
     , document.getElementById('root'));
 
