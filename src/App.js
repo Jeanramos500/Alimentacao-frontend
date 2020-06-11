033127218744f7b9a9c5eb075642615c5c45c347
Jeanramos500 <Jean_2009.ramos@hotmail.com>
2020-06-11 22:24:34 +0000

    front-end2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, {useState,useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import api from './api';
 import Button from '@material-ui/core/Button';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
 
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -11,12 +15,15 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Dialog from '@material-ui/core/Dialog';
+import TextField from '@material-ui/core/TextField';
+
 
 function App() {
 
     const [lista, setLista] = useState([]); //Imutabilidade
     const [loading, setLoading] = useState(true);
-
+    const [open, setOpen] = useState(false);
  
     //executa para obeter informações externas.
     useEffect(()=>{
@@ -26,6 +33,14 @@ function App() {
             setLoading(false);
         })
     },[])
+
+    function openModal(){
+        setOpen(true);
+    }
+
+      function closeModal(){
+        setOpen(false);
+    }
     
     return (
     <div style={{marginTop: '80px'}}>
@@ -42,9 +57,33 @@ function App() {
                  ))}
              </TableBody>
         </Table>
-        <Link to="/create">Adicionar</Link><Button variant="contained" color="primary">
-            Primary
+        <Button onClick={openModal} variant="contained" color="primary">
+            Adicionar
         </Button>
+        <Dialog open={open} onClose={closeModal}>
+            <DialogTitle id="form-dialog-title">Adicionar</DialogTitle>
+             <DialogContent>
+                <DialogContentText>
+                Digite a Alimentação correta
+            </DialogContentText>
+            <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Alimento"
+                type="email"
+                fullWidth
+            />
+            </DialogContent>
+            <DialogActions>
+            <Button onClick={closeModal} color="primary">
+                Cancelar
+            </Button>
+            <Button onClick={closeModal} color="primary">
+                Salvar
+            </Button>
+            </DialogActions>
+        </Dialog>
     </div>
     );
         }
