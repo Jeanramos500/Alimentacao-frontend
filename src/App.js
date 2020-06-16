826e0973bbff24f8c88d5e01ca2beac3eb1d0186
Jeanramos500 <60949369+Jeanramos500@users.noreply.github.com>
2020-06-16 19:56:35 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
-
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
